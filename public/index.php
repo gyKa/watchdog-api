1b1de99f4ec466a894e27fadfadb64adb6f09b4a
Gytis Karčiauskas <gytis@karciauskas.lt>
2017-11-08 22:00:04 +0200

    Add main dummy route.

--- public/index.php
+++ public/index.php
@@ -2,10 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 
 $app->get('/', function () {
     return time();
+});
+
+$app->post('/collect', function (Request $request) {
+    return new Response(null, 201);
 });
 
 $app->run();
